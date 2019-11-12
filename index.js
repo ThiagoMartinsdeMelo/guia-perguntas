582a6c8a0b355910b57e42a1b1e236ba96c7552f
thiago <thiagompdi@gmail.com>
2019-11-12 10:37:23 -0300

    Instalando Sequelize

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 // motor de html
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+// traduz os dados enviados pelo formulario em javascript
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
+
+// rotas
 app.get('/', (req, res) => {
     res.render('index');
 });
 
 app.get('/perguntar', (req, res) => {
     res.render('perguntar');
+});
+
+app.post('/salvarpergunta', (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send('Formulario recebido!');
 });
 
 app.listen(8080, () => {
